14baa56e317396e062efd39c1a450bd500488cf5
rahmadverii <rahmadveri2646@gmail.com>
2025-06-15 17:16:22 +0700

    Update sistem informasi mahasiswa.cpp

--- sistem informasi mahasiswa.cpp
+++ sistem informasi mahasiswa.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>   
-#include <cstring> // ini sbnrnya gpke jg masi bisa gt loh codingannya, kek nya,  tpi gktau basing lorang
+#include <vector>
+#include <cstring>
 using namespace std;
 
 struct MataKuliah {
@@ -8,23 +8,25 @@ struct MataKuliah {
     int sks;
     int prioritas;
 
-MataKuliah(const char* n = "", int s = 0, int p = 0){
-    strncpy(nama, n, sizeof(nama) - 1);
-nama[sizeof(nama)-1]='\0';
-sks = s;
-prioritas = p;
-}
+    MataKuliah(const char* n = "", int s = 0, int p = 0) {
+        strncpy(nama, n, sizeof(nama) - 1);
+        nama[sizeof(nama) - 1] = '\0';
+        sks = s;
+        prioritas = p;
+    }
 };
-struct Mahasiswa{
-char nama[50];
-string npm;
+
+struct Mahasiswa {
+    char nama[50];
+    string npm;
 };
+
 class Matkul {
 public:
     vector<Mahasiswa*> daftarMhs;
-    vector<Mahasiswa*> daftarMAtkul;
+    vector<MataKuliah*> daftarMatkul;
 
-       ~Matkul() {
+    ~Matkul() {
         for (auto mhs : daftarMhs) delete mhs;
         for (auto mk : daftarMatkul) delete mk;
     }
@@ -33,59 +35,50 @@ public:
         int pilih;
         while (true) {
             cout << "\n=== Sistem Informasi Mahasiswa ===" << endl;
-            cout << "1. Daftar Mahasigma" << endl;
-            cout << "2. Tambahkan mata kuliah" << endl;
-            cout << "3. Tampilkan semua data" << endl;
-            cout << "4. Rekomendasi Mata KUliah"<<endl;
-            cout << "5. Keluar" << endl;
+            cout << "1. Daftar Mahasiswa" << endl;
+            cout << "2. Tambah Mata Kuliah" << endl;
+            cout << "3. Tampilkan Semua Data" << endl;
+            cout << "4. Rekomendasi Mata Kuliah (Knapsack)" << endl;
+            cout << "5. Urutkan Mata Kuliah berdasarkan Prioritas (Merge Sort)" << endl;
+            cout << "6. Keluar" << endl;
             cout << "Pilih : ";
             cin >> pilih;
             cin.ignore();
 
             if (pilih == 1) {
                 Mahasiswa* mhs = new Mahasiswa;
-                cout << "Masukan nama : ";
+                cout << "Masukkan nama: ";
                 cin.getline(mhs->nama, 50);
-
-                cout << "Masukan npm : ";
+                cout << "Masukkan NPM: ";
                 cin >> mhs->npm;
                 cin.ignore();
-
-                daftarMhs.push_back(mhs);  //masukin 1 data mhs yg baru ke dlm daftarMhs
-
-               
+                daftarMhs.push_back(mhs);
 
             } else if (pilih == 2) {
                 char nama[50];
                 int sks, prioritas;
-
-                cout << "Masukan matakuliah: ";
+                cout << "Masukkan nama matkul: ";
                 cin.getline(nama, 50);
-
-                cout << "Masukan SKS: ";
+                cout << "Masukkan SKS: ";
                 cin >> sks;
-                cout << "MAsukkan Prioritas(1-10): ";
+                cout << "Masukkan prioritas (1-10): ";
                 cin >> prioritas;
                 cin.ignore();
 
-                daftarMatkul.push_back(new MataKuliah(nama, sks, prioritas));  
-                
-                // push_back tu buat nambah data ke belakang vector
-
-              
+                daftarMatkul.push_back(new MataKuliah(nama, sks, prioritas));
 
             } else if (pilih == 3) {
-                cout << "\n--- Semua Data Mahasiswa ---" << endl;
-                for (int i = 0; i < daftarMhs.size(); ++i) { // nampilin dta dri awal smpe akhir, klo gapke size() kita gak tau harus ulang berapa kali, karena jumlah datanya bisa berubah-ubah.
-                    cout << i + 1 << ". Nama: " << daftarMhs[i]->nama // biar penomorannya dari 1 bukan 0
+                cout << "\n--- Daftar Mahasiswa ---" << endl;
+                for (int i = 0; i < daftarMhs.size(); ++i) {
+                    cout << i + 1 << ". Nama: " << daftarMhs[i]->nama
                          << ", NPM: " << daftarMhs[i]->npm << endl;
                 }
 
-                cout << "\n--- Daftar MAta KUliah ---" << endl;
-                for (int i = 0; i < daftarMatkul.size(); ++i) { //sama
-                    cout << i + 1 << ". " << daftarMatkul[i]->nama  // sama
-                         << " - SKS: " << daftarSKS[i]->sks
-                        << " - Prioritas: " << daftarMatkul[i]->prioritas << endl;
+                cout << "\n--- Daftar Mata Kuliah ---" << endl;
+                for (int i = 0; i < daftarMatkul.size(); ++i) {
+                    cout << i + 1 << ". " << daftarMatkul[i]->nama
+                         << " | SKS: " << daftarMatkul[i]->sks
+                         << " | Prioritas: " << daftarMatkul[i]->prioritas << endl;
                 }
 
             } else if (pilih == 4) {
@@ -101,39 +94,41 @@ public:
 
             } else {
                 cout << "Pilihan tidak valid.\n";
+            }
         }
     }
-}
-void rekomendasiMatkul(){
-int n = daftarMatkul.size():
-int kapasitasSKS;
-cout << "Masukkan batas maksimal SKS: ";
-cin >> kapasitasSKS;
 
-vector<vector<int>>dp(n+1,vector<int>(kapasitasSKS + 1, 0));
-for (int i = 1; i<= n; ++i){
-int w = daftarMatkul[i-1]->sks;
-int v = daftarMatkul[i-1]->prioritas;
-for (int j = 0; j<=kapasitasSKS;++j){
-if(w<=j)
-dp[i][j] = max(dp[i-1[j],dp[i-1[j-w]+v);
-else 
-    dp[i][j] = dp[i-1][j];
-}
-}
-cout << "\n >> total prioritas maksimal: " << dp[n] [kapasitasSKS]<< endl;
-cout << " >> MAtkul yang direkomendasikan:\n";
+    void rekomendasiMatkul() {
+        int n = daftarMatkul.size();
+        int kapasitasSKS;
+        cout << "Masukkan batas maksimal SKS: ";
+        cin >> kapasitasSKS;
 
-int j = kapasitasSKS;
-for (int i = n; i>0 && j > 0; --i){
-if (dp[i][j]!= dp [i-1][j]){
-MataKuliah* mk = daftarMatkul[i-1];
-cout << "- "<<mk->nama<<" (SKS: " << mk->sks
-    <<",Prioritas: " << mk->prioritas << ")\n";
-j-=mk->sks;
-}
-}
-}
+        vector<vector<int>> dp(n + 1, vector<int>(kapasitasSKS + 1, 0));
+
+        for (int i = 1; i <= n; ++i) {
+            int w = daftarMatkul[i - 1]->sks;
+            int v = daftarMatkul[i - 1]->prioritas;
+            for (int j = 0; j <= kapasitasSKS; ++j) {
+                if (w <= j)
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        cout << "\n>> Total prioritas maksimal: " << dp[n][kapasitasSKS] << endl;
+        cout << ">> Matkul yang direkomendasikan:\n";
+
+        int j = kapasitasSKS;
+        for (int i = n; i > 0 && j > 0; --i) {
+            if (dp[i][j] != dp[i - 1][j]) {
+                MataKuliah* mk = daftarMatkul[i - 1];
+                cout << "- " << mk->nama << " (SKS: " << mk->sks << ", Prioritas: " << mk->prioritas << ")\n";
+                j -= mk->sks;
+            }
+        }
+    }
 
     // === Merge Sort: Divide & Conquer berdasarkan PRIORITAS tertinggi ===
     void mergeSortMatkul(int left, int right) {
@@ -164,6 +159,5 @@ j-=mk->sks;
 int main() {
     Matkul app;
     app.menu();
-
     return 0;
 }
